363c4d256d2de4f04c6c647c355d853a0999ced9
MySandyGit <santhoshkumarpsk2972@gmail.com>
2025-12-20 19:46:51 +0530

    FirstCommit

---
+++ projectWork/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module projectWork {
+}
---
+++ projectWork/src/projectWork/helloBuddy.java
@@ -0,0 +1,9 @@
+package projectWork;
+
+public class helloBuddy {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+}
